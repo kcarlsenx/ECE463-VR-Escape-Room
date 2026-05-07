9ae142a50c30fcf8764cf6deabc15e786c803a5d
kcarlsenx <kcarlsen@arizona.edu>
2026-05-07 14:51:29 -0700

    Buttons, Broken Wall, Oven Knobs
    The buttons now have correct colliders. The recipe book no longer crash unity. Added sound to drawer opening. Added a missing box collider that allowed player to fall into void. Fixed objects clipping in from main room. Made oven knobs interactable in VR.

--- Assets/Scripts/OpenCabinet.cs
+++ Assets/Scripts/OpenCabinet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class OpenCabinet : MonoBehaviour
@@ -11,14 +12,17 @@ public class OpenCabinet : MonoBehaviour
 
     private Vector3 closedPosition;
     private bool opened = false;
+    private float timer;
+    public AudioSource cabinentAudio;
+    public AudioClip cabinentMoveClip;
 
     private void Start()
     {
         if (cabinetDoorOrDrawer != null)
             closedPosition = cabinetDoorOrDrawer.localPosition;
 
-        if (objectToReveal != null)
-            objectToReveal.SetActive(false);
+        if (cabinentAudio != null)
+            cabinentAudio = GetComponent<AudioSource>();
     }
 
     public void Open()
@@ -26,12 +30,33 @@ public class OpenCabinet : MonoBehaviour
         if (opened || cabinetDoorOrDrawer == null) return;
 
         cabinetDoorOrDrawer.localPosition = closedPosition + openOffset;
+        playCabinentSound();
 
         if (objectToReveal != null)
-            objectToReveal.SetActive(true);
+            timer += Time.deltaTime;
+
+            if (timer >= 1f)
+            {
+            objectToReveal.transform.position = new Vector3(
+                2.8944259f,
+                0.8840437f,
+                -3.900727f);        
+            }
+    
 
         opened = true;
 
         Debug.Log("Cabinet opened");
     }
+
+    private void playCabinentSound()
+    {
+        if (cabinentAudio != null && cabinentMoveClip != null)
+        {
+            cabinentAudio.pitch = Random.Range(0.9f, 1.1f);
+            cabinentAudio.PlayOneShot(cabinentMoveClip);
+        }
+    }
+
 }
+
