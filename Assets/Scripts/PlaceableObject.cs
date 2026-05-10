78810ffb4e91de1af12b86f9c7ecc19d330b3783
kcarlsenx <kcarlsen@arizona.edu>
2026-05-09 19:18:47 -0700

    Finished Puzzle 2
    Placement puzzle finished

---
+++ Assets/Scripts/PlaceableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableObject : MonoBehaviour
+{
+    public string objectID;
+
+    public bool isPlacedCorrectly = false;
+}
---
+++ Assets/Scripts/PlacementPuzzle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlacementPuzzle : MonoBehaviour
+{
+    public List<bool> currentPuzzleState = new List<bool> { false, false, false, false }; // current correct placements
+    public UnityEvent puzzleSolved;
+
+    public void updateCorrect(int currPlacement)
+    {
+        currentPuzzleState[currPlacement] = true;
+        checkComplete();
+    }
+
+    public void updateIncorrect(int currPlacement)
+    {
+        currentPuzzleState[currPlacement] = false;
+        checkComplete();
+    }
+
+    private void checkComplete()
+    {
+    // Check every puzzle state entry
+    foreach (bool state in currentPuzzleState)
+    {
+        if (!state)
+        {
+            return;
+        }
+    }
+
+    Debug.Log("Puzzle Solved!");
+
+    puzzleSolved.Invoke();
+    }
+
+   
+}
